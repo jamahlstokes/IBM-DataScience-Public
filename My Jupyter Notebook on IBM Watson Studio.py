462b00864312550c4b6e450d04ff9cc74a1c35a9
jamahlstokes <114500526+jamahlstokes@users.noreply.github.com>
2022-09-29 10:58:43 -0400

    Add 6 cell notebook via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> My Jupyter Notebook on IBM Watson Studio <h1>
+
+# <b>Jamahl Stokes</b> <br>
+# Data Scientist
+
+# <i> I am interested in data science because I enjoy using math to solve interesting problems. I see data science as a way to explore my interests in finance, economics, and public policy. </i>
+
+# <h3> The code below multiplies finds of the product of 1 through 3. <h3>
+
+# In[7]:
+
+
+print("1*2*3=") 
+1*2*3 
+
+
+# <blockquote> Priorities for a good day </blockquote>
+# <hr>
+# <ol>
+# <li>Sleep</li>
+# <li>Eat</li>
+# <li>Relax</li>
+# </ol>
+
+# In[ ]:
+
+
+
+
